2647c21bfa618a2efa93d0d445aa71cc43cf2e11
vukadinovic22 <vukadinovicaleksa22@gmail.com>
2021-07-26 10:12:36 +0200

    Add project files.

---
+++ Prvi/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Prvi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(200, 117);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(372, 56);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kreiraj dugme";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Prvi/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prvi
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            Form1.dogadjaj += kreirajDugmice;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void kreirajDugmice(int broj)
+        {
+            this.Controls.Clear();
+            Random rnd = new Random();
+            for (int i = 0; i < broj; i++)
+            {
+                Button b = new Button();
+                b.Left = rnd.Next(this.ClientSize.Width - b.Width);
+                b.Top = rnd.Next(this.ClientSize.Height - b.Height);
+                b.Text = (i + 1).ToString();
+                this.Controls.Add(b);
+            }
+
+        }
+    }
+}
